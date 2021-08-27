3b109cd4fa34b8e0401e3c592c149eb1c3d6d599
Mooventhan <kmooventhan98@gmail.com>
2021-08-27 19:42:48 +0530

    Updating article added view and change in contorller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,24 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article=Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice]="Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@article=Article.find(params[:id])
 	end
 
+
+	def edit
+		@article = Article.find(params[:id])
+	end
 
 	private
 		def article_params
