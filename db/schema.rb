3ea5e2c6f26743fc1762544a5a2d6ff84b8a0a5f
Mooventhan <kmooventhan98@gmail.com>
2021-09-23 09:27:49 +0530

    category model unit test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210919163348) do
+ActiveRecord::Schema.define(version: 20210922021510) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20210919163348) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
